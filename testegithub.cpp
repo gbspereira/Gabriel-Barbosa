882dea48dc9543758c3ec2343459eb5048bd61e9
gbspereira <69473713+gbspereira@users.noreply.github.com>
2020-08-10 14:22:15 -0300

    Add files via upload

---
+++ testegithub.cpp
@@ -0,0 +1,20 @@
+#include <iostream>;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	string n;
+	int x, z, soma;
+	
+	cout << "Digite o primeiro numero a ser somado ";
+	cin >> x;
+	cout << "Digite o segundo numero a ser somado ";
+	cin >> z;
+	soma = x + z;
+	
+	cout << "A soma dos dois numeros sera: " << soma;
+	
+	
+	
+	return 0;
+}
